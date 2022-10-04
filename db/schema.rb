7e5adae1ce08957711c3880bd98ecb66fec02d91
Paul <paul.kiplagt@student.moringaschool.com>
2022-10-04 19:37:31 +0300

    solved

--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_122602) do
+ActiveRecord::Schema.define(version: 2022_10_04_162435) do
 
-    create_table "activities", force: :cascade do |t|
-      t.string "name"
-      t.integer "difficulty"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-    end
-  
-    create_table "campers", force: :cascade do |t|
-      t.string "name"
-      t.integer "age"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-    end
-  
-    create_table "signups", force: :cascade do |t|
-      t.integer "camper_id", null: false
-      t.integer "activity_id", null: false
-      t.integer "time"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.index ["activity_id"], name: "index_signups_on_activity_id"
-      t.index ["camper_id"], name: "index_signups_on_camper_id"
-    end
-  
-    add_foreign_key "signups", "activities"
-    add_foreign_key "signups", "campers"
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.integer "difficulty"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "campers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "signups", force: :cascade do |t|
+    t.integer "camper_id", null: false
+    t.integer "activity_id", null: false
+    t.integer "time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["activity_id"], name: "index_signups_on_activity_id"
+    t.index ["camper_id"], name: "index_signups_on_camper_id"
+  end
+
+  add_foreign_key "signups", "activities"
+  add_foreign_key "signups", "campers"
+end
